aa1c036147a29c8f067edb66169a299ab68194ba
asukachikaru <asukachikaru@gmail.com>
2022-01-13 23:24:23 +0900

    feat: basic server cache

---
+++ src/server/apiCacheController.ts
@@ -0,0 +1,31 @@
+import {
+  NotionPageChildrenResponse,
+  NotionPageListResponse,
+} from "client/types/notion";
+
+type CacheControllerItem<T> = {
+  data?: T;
+  lastUpdated: string;
+};
+
+export class CacheController {
+  constructor() {}
+
+  get(key: "postList" | "postDetail") {
+    return this[key].data;
+  }
+
+  set(key: "postList" | "postDetail", data: any) {
+    this[key].data = data;
+  }
+
+  private postList: CacheControllerItem<NotionPageListResponse> = {
+    data: undefined,
+    lastUpdated: "",
+  };
+
+  private postDetail: CacheControllerItem<NotionPageChildrenResponse> = {
+    data: undefined,
+    lastUpdated: "",
+  };
+}
--- src/server/notionApiServices.ts
+++ src/server/notionApiServices.ts
@@ -3,10 +3,13 @@ import {
   NotionPageChildrenResponse,
   NotionPageListResponse,
 } from "client/types/notion";
+import { CacheController } from "./apiCacheController";
 
 const NOTION_API_TOKEN = process.env.NOTION_API_TOKEN;
 const NOTION_DATABASE_ID = process.env.NOTION_DATABASE_ID;
 const NOTION_API_BASEURL = "https://api.notion.com/v1";
+
+const cacheController = new CacheController();
 
 export const axiosInstance = axios.create({
   baseURL: NOTION_API_BASEURL,
@@ -31,16 +34,28 @@ export const postListFilterSorter = {
 };
 
 export const getNotionPageList = async () => {
+  if (cacheController.get("postList")) {
+    return cacheController.get("postList") as NotionPageListResponse;
+  }
+
   const response = await axiosInstance.post<NotionPageListResponse>(
     `${NOTION_API_BASEURL}/databases/${NOTION_DATABASE_ID}/query`,
     postListFilterSorter
   );
+  cacheController.set("postList", response.data);
+
   return response.data;
 };
 
 export const getNotionBlockList = async (postId: string) => {
+  if (cacheController.get("postDetail")) {
+    return cacheController.get("postDetail") as NotionPageChildrenResponse;
+  }
+
   const response = await axiosInstance.get<NotionPageChildrenResponse>(
     `${NOTION_API_BASEURL}/blocks/${postId}/children`
   );
+  cacheController.set("postDetail", response.data);
+
   return response.data;
 };
