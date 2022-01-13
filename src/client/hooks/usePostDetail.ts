b51d9c5d5deeff78c242fdb07959061da003e43d
asukachikaru <asukachikaru@gmail.com>
2022-01-13 22:31:55 +0900

    fix: post detail fetch enable

--- src/client/hooks/usePostDetail.ts
+++ src/client/hooks/usePostDetail.ts
@@ -3,5 +3,7 @@ import { useQuery } from "react-query";
 import { getPostDetail, POST_DETAIL_QUERY_KEY } from "client/services/apiCore";
 
 export const usePostDetail = (id: string) => {
-  return useQuery([POST_DETAIL_QUERY_KEY, id], () => getPostDetail(id));
+  return useQuery([POST_DETAIL_QUERY_KEY, id], () => getPostDetail(id), {
+    enabled: !!id,
+  });
 };
