eb873158307591e65a71b22aa689144b486fbb46
asukachikaru <asukachikaru@gmail.com>
2022-01-14 23:58:09 +0900

    feat: postlink

--- src/client/components/PostLink.tsx
+++ src/client/components/PostLink.tsx
@@ -1,7 +1,9 @@
 import * as React from "react";
 import { Link } from "react-router-dom";
+import styled from "styled-components";
 
 import { PostSummary } from "client/types";
+import { dateParser } from "client/utils/parsers";
 
 interface OwnProps {
   postSummary: PostSummary;
@@ -9,13 +11,75 @@ interface OwnProps {
 
 export const PostLink: React.VFC<OwnProps> = ({ postSummary }) => {
   return (
-    <div>
-      <Link to={`/post/${postSummary.pathname}`}>{postSummary.title}</Link>
-      <div style={{ display: "flex" }}>
+    <StyledContainer>
+      <StyledPostTitle to={`/post/${postSummary.pathname}`}>
+        {postSummary.title}
+      </StyledPostTitle>
+      {/** todo: lan */}
+      <StyledPostPublishDate>
+        {dateParser(postSummary.publishedDate)}
+      </StyledPostPublishDate>
+      <StyledPostTagContainer style={{ display: "flex" }}>
         {postSummary.tags.map((tag) => (
-          <span key={tag}>{tag}</span>
+          <StyledPostTag to={`/tag/${tag}`} key={tag}>
+            #{tag}
+          </StyledPostTag>
         ))}
-      </div>
-    </div>
+      </StyledPostTagContainer>
+    </StyledContainer>
   );
 };
+
+const StyledContainer = styled.div`
+  margin-bottom: 40px;
+  display: flex;
+  flex-direction: column;
+`;
+
+const StyledPostTitle = styled(Link)`
+  font-size: 35px;
+  line-height: 50px;
+
+  @media (max-width: 400px) {
+    font-size: 25px;
+    line-height: 35px;
+  }
+`;
+
+const StyledPostPublishDate = styled.div`
+  margin-top: 15px;
+  line-height: 1;
+  font-family: "Noto Serif JP", serif;
+  font-size: 20px;
+  color: #7a7a7a;
+
+  @media (max-width: ${(props) => props.theme.breakpoint.mobile}) {
+    margin-right: 10px;
+    font-size: 15px;
+  }
+`;
+
+const StyledPostTagContainer = styled.div`
+  margin-top: 15px;
+  line-height: 1;
+
+  @media (max-width: ${(props) => props.theme.breakpoint.mobile}) {
+    margin-top: 10px;
+  }
+`;
+
+const StyledPostTag = styled(Link)`
+  margin-right: 15px;
+  font-weight: 100;
+  font-family: "Noto Sans JP", sans-serif;
+  font-size: 20px;
+
+  &:hover {
+    text-decoration: underline;
+  }
+
+  @media (max-width: ${(props) => props.theme.breakpoint.mobile}) {
+    margin-right: 10px;
+    font-size: 15px;
+  }
+`;
