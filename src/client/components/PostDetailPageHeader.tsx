d85a4bd3376277f7c0037e1b495000f0c8b1f437
asukachikaru <asukachikaru@gmail.com>
2022-01-15 00:05:30 +0900

    feat: post detail page header

--- src/client/components/PostDetailPageHeader.tsx
+++ src/client/components/PostDetailPageHeader.tsx
@@ -1,6 +1,9 @@
 import * as React from "react";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
 
 import { PostSummary } from "client/types";
+import { dateParser } from "client/utils/parsers";
 
 interface OwnProps {
   postSummary: PostSummary;
@@ -8,14 +11,65 @@ interface OwnProps {
 
 export const PostDetailPageHeader: React.VFC<OwnProps> = ({ postSummary }) => {
   return (
-    <div>
-      <h1>{postSummary.title}</h1>
-      <h4>{postSummary.publishedDate}</h4>
-      <div style={{ display: "flex" }}>
+    <StyledContainer>
+      <StyledTitle>{postSummary.title}</StyledTitle>
+      {/** todo: lan */}
+      <StyledPublishDate>
+        {dateParser(postSummary.publishedDate)}
+      </StyledPublishDate>
+      <StyledTagContainer>
         {postSummary.tags.map((tag) => (
-          <span key={tag}>{tag}</span>
+          <StyledTag to={`/tag/${tag}`} key={tag}>
+            #{tag}
+          </StyledTag>
         ))}
-      </div>
-    </div>
+      </StyledTagContainer>
+    </StyledContainer>
   );
 };
+
+const StyledContainer = styled.div`
+  margin-bottom: 40px;
+`;
+
+const StyledTitle = styled.h1`
+  font-size: 50px;
+  line-height: 70px;
+  font-weight: 700;
+
+  @media (max-width: ${(props) => props.theme.breakpoint.mobile}) {
+    font-size: 30px;
+    line-height: 40px;
+  }
+`;
+
+const StyledPublishDate = styled.div`
+  margin-top: 5px;
+  font-size: 20px;
+  color: #7a7a7a;
+  font-family: "Noto Serif JP", sans-serif;
+`;
+
+const StyledTagContainer = styled.div`
+  margin-top: 15px;
+  line-height: 1;
+`;
+
+const StyledTag = styled(Link)`
+  padding: 0 10px;
+  font-weight: 100;
+  font-family: "Noto Sans JP", sans-serif;
+  font-size: 20px;
+
+  &:first-of-type {
+    padding-left: 0;
+  }
+
+  &:hover {
+    text-decoration: underline;
+  }
+
+  @media (max-width: ${(props) => props.theme.breakpoint.mobile}) {
+    font-size: 15px;
+  }
+`;
